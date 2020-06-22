95895705a1fe1c656cb1cb0d96249adea9f2ae6b
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-06-22 19:24:59 -0300

    new webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,26 @@
-'use strict';
-
-const webpack = require('webpack');
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-
   entry: './src/index.js',
-
   output: {
+    filename: 'index.js',
     path: path.resolve(__dirname, 'build'),
-    publicPath: '/build/',
-    filename: 'project.bundle.js'
   },
 
   module: {
     rules: [
-       {
+      {
+        test: [/\.vert$/, /\.frag$/],
+        use: require.resolve('raw-loader'),
+      },
+      {
+        test: /\.(png|jpg|svg|gig)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
         test: /\.css$/,
         use: [
           'style-loader',
@@ -23,23 +28,30 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|svg|jpg|gif|jpeg|)$/,
-        use: [
-          'file-loader',
-        ],
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        include: path.resolve(__dirname, 'src/'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env', {
+                  targets: {
+                    node: 'current',
+                  },
+                },
+              ],
+            ],
+          },
+        },
       },
-      {
-        test: [/\.vert$/, /\.frag$/],
-        use: 'raw-loader'
-          }
-        ]
+    ],
   },
-
   plugins: [
-        new webpack.DefinePlugin({
-      'CANVAS_RENDERER': JSON.stringify(true),
-      'WEBGL_RENDERER': JSON.stringify(true)
-    })
-    ]
-
+    new webpack.DefinePlugin({
+      CANVAS_RENDERER: JSON.stringify(true),
+      WEBGL_RENDERER: JSON.stringify(false),
+    }),
+  ],
 };
