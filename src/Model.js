ad66b64736dcdc13b7dc0557f238a08d3834cf7b
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-06-22 23:31:52 -0300

    get is working on test

--- src/Model.js
+++ src/Model.js
@@ -1,42 +1,75 @@
-const model = (() => {
+import axios from 'axios';
 
-  const baseApiUrl = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/x2mheK9ynuj1B6F38TME/scores";
+const baseApiUrl = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/x2mheK9ynuj1B6F38TME/scores";
 
-  const getRequest = () => {
-    let response = ''
-    fetch(`${baseApiUrl}`, {
-      mode: 'cors',
-    }).then((resp) => resp.json()).then((resp) => {
-      updateScoreBoard(resp);
-    }).catch(function (error) {});
-  };
-
-  const updateScoreBoard = (resp) => {
-    let i = 0
-    while (i < resp.result['length']) {
-      document.getElementById('ul').insertAdjacentHTML('afterbegin', `<li class='li'><span>${resp.result[i].user}</span><span>${resp.result[i].score}</span></li>`);
-      i += 1;
-    }
-    return resp;
-  };
-
-  const postRequest = async (gameOverData) => {
-    const raw = await fetch(`${baseApiUrl}`, {
-      mode: 'cors',
-      method: 'POST',
-      body: gameOverData,
-      headers: {
-        'Content-Type': 'application/json'
-      }
+const postRequest = async (gameOverData) => {
+  try {
+    const result = await axios.post(baseApiUrl, {
+      gameOverData
     });
-    const response = raw;
-    return response;
-  };
-  
-  return {
-    getRequest,
-    postRequest
-  };
-})();
+    return result;
+  } catch (error) {
+    throw new Error('Unable to add score to board');
+  }
+};
 
-export default model
+const updateScoreBoard = (resp) => {
+ let i = 0
+ while (i < resp.result['length']) {
+   document.getElementById('ul').insertAdjacentHTML('afterbegin', `<li class='li'><span>${resp.result[i].user}</span><span>${resp.result[i].score}</span></li>`);
+   i += 1;
+ }
+ return resp;};
+
+
+const getRequest = async () => {
+  try {
+    const data = await axios.get(`${baseApiUrl}`);
+    return data;
+  } catch (error) {
+    throw new Error('Unable to get scores at this time');
+  }
+};
+
+export {getRequest, postRequest, updateScoreBoard};
+
+//const model = (() => {
+//
+//  const baseApiUrl = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/x2mheK9ynuj1B6F38TME/scores";
+//
+//  const getRequest = () => {
+//    let response = ''
+//    fetch(`${baseApiUrl}`, {
+//      mode: 'cors',
+//    }).then((resp) => resp.json()).then((resp) => {
+//      updateScoreBoard(resp)
+//    }).catch(function (error) {});
+//  };
+//
+//  const updateScoreBoard = (resp) => {
+//    let i = 0
+//    while (i < resp.result['length']) {
+//      document.getElementById('ul').insertAdjacentHTML('afterbegin', `<li class='li'><span>${resp.result[i].user}</span><span>${resp.result[i].score}</span></li>`);
+//      i += 1;
+//    }
+//    return resp;
+//  };
+//
+//  const postRequest = async (gameOverData) => {
+//    const raw = await fetch(`${baseApiUrl}`, {
+//      mode: 'cors',
+//      method: 'POST',
+//      body: gameOverData,
+//      headers: {
+//        'Content-Type': 'application/json'
+//      }
+//    });
+//    const response = raw;
+//    return response;
+//  };
+//
+//  return {
+//    getRequest,
+//    postRequest
+//  };
+//})();
--- test/Model.test.js
+++ test/Model.test.js
@@ -1,26 +1,14 @@
-import "core-js/stable";
+//import "core-js/stable";
 import "regenerator-runtime/runtime";
-import model from '../src/Model';
+import { getRequest } from '../src/Model';
 
 
-test('getRequest will return a json with users submitted scores', () => {
-  const dummyMoviesData = [
-    {
-      user: 'some-tilte-1',
-      score: 'some-1'
-    },
-    {
-      user: 'some-tilte-2',
-      score: 'some-2'
-    },
-    {
-      user: 'some-tilte-3',
-      score: 'some-3'
-    }
-];
-  global.fetch = jest.fn(() => Promise.resolve(dummyMoviesData));
-  const resp = model.getRequest();
-  expect(resp).toBe(5);
-});
+test('getRequest to api const url should be able to GET', async() => {
+  const result = await getRequest();
+  expect(result.status).toBe(200);
+},3000);
 
-test('Calling call request with json formatted as {user: str:name, score: int:score} should update the scores db', () => {});
+test('getRequest to api const url should return a JSON with format {user: str, score:int}', async() => {
+  const result = await getRequest();
+  expect(result.data).toBe(200);
+},3000);
