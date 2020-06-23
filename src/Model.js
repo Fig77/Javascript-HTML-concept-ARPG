4fbaac02fbdfdaa2932498523d726346dfb2996f
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-06-22 23:54:16 -0300

    get and post to api working on test

--- src/Model.js
+++ src/Model.js
@@ -4,8 +4,9 @@ const baseApiUrl = "https://us-central1-js-capstone-backend.cloudfunctions.net/a
 
 const postRequest = async (gameOverData) => {
   try {
-    const result = await axios.post(baseApiUrl, {
-      gameOverData
+    const result = await axios.post(`${baseApiUrl}`, {
+      user: gameOverData.user,
+      score: gameOverData.score,
     });
     return result;
   } catch (error) {
@@ -32,44 +33,3 @@ const getRequest = async () => {
 };
 
 export {getRequest, postRequest, updateScoreBoard};
-
-//const model = (() => {
-//
-//  const baseApiUrl = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/x2mheK9ynuj1B6F38TME/scores";
-//
-//  const getRequest = () => {
-//    let response = ''
-//    fetch(`${baseApiUrl}`, {
-//      mode: 'cors',
-//    }).then((resp) => resp.json()).then((resp) => {
-//      updateScoreBoard(resp)
-//    }).catch(function (error) {});
-//  };
-//
-//  const updateScoreBoard = (resp) => {
-//    let i = 0
-//    while (i < resp.result['length']) {
-//      document.getElementById('ul').insertAdjacentHTML('afterbegin', `<li class='li'><span>${resp.result[i].user}</span><span>${resp.result[i].score}</span></li>`);
-//      i += 1;
-//    }
-//    return resp;
-//  };
-//
-//  const postRequest = async (gameOverData) => {
-//    const raw = await fetch(`${baseApiUrl}`, {
-//      mode: 'cors',
-//      method: 'POST',
-//      body: gameOverData,
-//      headers: {
-//        'Content-Type': 'application/json'
-//      }
-//    });
-//    const response = raw;
-//    return response;
-//  };
-//
-//  return {
-//    getRequest,
-//    postRequest
-//  };
-//})();
--- test/Model.test.js
+++ test/Model.test.js
@@ -1,14 +1,38 @@
 //import "core-js/stable";
+import {
+  matchers
+} from 'jest-json-schema';
+expect.extend(matchers);
 import "regenerator-runtime/runtime";
-import { getRequest } from '../src/Model';
+import {
+  getRequest,
+  postRequest
+} from '../src/Model';
 
 
-test('getRequest to api const url should be able to GET', async() => {
+test('getRequest to api const url should be able to GET', async () => {
   const result = await getRequest();
   expect(result.status).toBe(200);
-},3000);
+}, 3000);
 
-test('getRequest to api const url should return a JSON with format {user: str, score:int}', async() => {
+test('getRequest to api const url should return a JSON with format {user: str, score:int}', async () => {
   const result = await getRequest();
-  expect(result.data).toBe(200);
-},3000);
+  const schema = {
+    properties: {
+      user: {
+        type: 'string'
+      },
+      score: {
+        type: 'integer'
+      }
+    },
+    required: [],
+  };
+  expect(result.data).toMatchSchema(schema);
+}, 3000);
+
+test('postRequest will send succesfuly a POST to the api const url', async()=> {
+  const data = await postRequest({user: 'Tester-1', score: 10});
+  expect(data.status).toBe(201);
+  expect(data.statusText).toBe('Created');
+}, 3000); 
